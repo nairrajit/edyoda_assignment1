339b9370d6018917b7827596a5e040061d5bd6fd
nairrajit <109478936+nairrajit@users.noreply.github.com>
2022-07-17 23:33:12 +0530

    initial commit

---
+++ even_odd.py
@@ -0,0 +1,16 @@
+lst=[]
+num=int(input("Enter the number of digits in the series: "))
+for n in range(num):
+        series= int(input("Enter the numbers of the series:"))
+        lst.append(series)
+               
+even, odd=0,0
+for i in lst:
+    if  i%2==0:
+      even +=1
+    else:
+      odd+=1
+
+print(lst)  
+print("Number of even digits: ",even)
+print("Number of odd digits: ",odd)
